cfec05e948a01f1c26112826eea25057fae10713
jacobrubin <jacobdr4@illinois.edu>
2019-12-03 10:44:03 -0600

    Note Selection Commit

--- app/src/main/java/com/example/notesapp/Note.java
+++ app/src/main/java/com/example/notesapp/Note.java
@@ -1,16 +1,22 @@
 package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import android.os.Bundle;
 
+@Entity
 public final class Note {
     private String title;
     private String text;
+    @PrimaryKey
+    private int id;
 
     public Note(String setTitle) {
         title = setTitle;
         text = "";
+        id = 0;
     }
     public void setText(String t) {
         text = t;
