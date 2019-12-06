f73959156adb566691e5034c774e6550f9103132
jacobrubin <jacobdr4@illinois.edu>
2019-12-06 16:10:41 -0600

    Data Storage Commit

--- app/src/main/java/com/example/notesapp/Menu.java
+++ app/src/main/java/com/example/notesapp/Menu.java
@@ -2,65 +2,105 @@ package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import com.example.notesapp.Room.DatabaseAccess;
+import com.example.notesapp.Room.Note;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.notesapp.MainActivity.appDatabase;
 
 public class Menu extends AppCompatActivity {
+    //Initialize the Room Database
+    DatabaseAccess noteDAO = appDatabase.getNoteDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_menu);
 
-        setUpUI();
+        List<Note> noteList = noteDAO.getNotes();
+
+        //deleteData(noteList);
+        //noteList = noteDAO.getNotes();
+
+        setUpUI(noteList);
     }
 
-    public void setUpUI() {
-        //TEST: Add 10 notes to list of notes
-        ArrayList<Note> noteList = new ArrayList<Note>();
-        for (int i = 0; i < 20; i++) {
-            noteList.add(new Note(i + "rd Note"));
+    public void deleteData(List<Note> noteList) {
+        //USE THIS WHEN DEMOING APP - DELETES ALL NOTES
+        for (int i = 0; i < noteList.size(); i++) {
+            noteDAO.deleteData(noteList.get(i));
         }
+    }
 
-        //Create list of buttons to scroll through
+    public void setUpUI(List<Note> noteList) {
+        //Create new note button
+        Button newNoteButton = findViewById(R.id.newNoteButton);
+        newNoteButton.setOnClickListener(unused -> newNoteButtonClicked(noteList));
+
+        //System.out.println(noteList);
+        //System.out.println("Notelist size: " + noteList.size());
+
+        //Create actual notes buttons
         LinearLayout buttonList = findViewById(R.id.buttonList);
-        for (int i = 0; i < noteList.size() + 1; i++) {
+        for (int i = 0; i < noteList.size(); i++) {
+            //System.out.println("Button Made");
             View buttonChunk = getLayoutInflater().inflate(R.layout.chunk_note,
                     buttonList, false);
-            Button noteButton = buttonChunk.findViewById(R.id.noteTitleButton);
-
-            //Adds the new note button or note button
-            if (i == 0) {
-                noteButton.setText("New Note");
-                noteButton.setOnClickListener(unused -> newNoteButtonClicked());
-            } else {
-                noteButton.setText("Note: " + i);
-                final String noteCtr = "Selected Note: " + i;
-                noteButton.setOnClickListener(unused -> noteButtonClicked(noteCtr));
-            }
+            Button noteButton = buttonChunk.findViewById(R.id.noteTextButton);
+            noteButton.setText(noteList.get(i).getTitle());
+            final int noteID = noteList.get(i).getId();
+            noteButton.setOnClickListener(unused -> noteButtonClicked(noteID));
             buttonList.addView(buttonChunk);
         }
     }
 
-    private void newNoteButtonClicked() {
+    private void newNoteButtonClicked(List<Note> noteList) {
         LinearLayout buttonList = findViewById(R.id.buttonList);
         View buttonChunk = getLayoutInflater().inflate(R.layout.chunk_note,
                 buttonList, false);
-        Button noteButton = buttonChunk.findViewById(R.id.noteTitleButton);
-        noteButton.setText("Recently Added Note");
-        noteButton.setOnClickListener(unused -> noteButtonClicked("Recently Added"));
+
+        //Add note to list, create new button and change title text
+        //FOR SOME REASON, WHEN THE NOTE IS FIRST CREATED IT HAS AN ID OF 42?!?!?! OR 47??!
+        noteList.add(new Note("Note #" + (noteList.size() + 1),"Edit Text Here", noteList.size() + 1));
+        Button noteButton = buttonChunk.findViewById(R.id.noteTextButton);
+        noteButton.setText(noteList.get(noteList.size() - 1).getTitle());
+
+        //Test dis shit
+        //System.out.println("Note0: " + noteList.get(0));
+        System.out.println(noteList);
+        System.out.println("Notelist size: " + noteList.size());
+
+        //Create onClickListener
+        int noteID = noteList.get(noteList.size() - 1).getId();
+        System.out.println("NoteID: " + noteID);
+
+        //Add note to Room
+        noteDAO.addNote(noteList.get(noteList.size() - 1));
+        System.out.println("Note successfully added to DAO");
+
+        noteButton.setOnClickListener(unused -> noteButtonClicked(noteID));
+
         buttonList.addView(buttonChunk);
     }
 
-    private void noteButtonClicked(String noteID) {
+    private void noteButtonClicked(int noteID) {
+        System.out.println("Note Clicked");
         Intent intent = new Intent(this, NoteDisplay.class);
         intent.putExtra("noteID", noteID);
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/notesapp/NoteDisplay.java
+++ app/src/main/java/com/example/notesapp/NoteDisplay.java
@@ -7,19 +7,64 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.notesapp.Room.DatabaseAccess;
+import com.example.notesapp.Room.Note;
+
 import org.w3c.dom.Text;
 
+import java.util.List;
+
+import static com.example.notesapp.MainActivity.appDatabase;
+
 public class NoteDisplay extends AppCompatActivity {
+    private Note receivedNote;
+    private EditText text;
+    private EditText title;
+    private DatabaseAccess noteDAO = appDatabase.getNoteDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_display);
 
-        Intent intent = getIntent();
-        String id = intent.getStringExtra("noteID");
+        System.out.println("Note Display Started");
 
-        EditText text = findViewById(R.id.noteText);
-        text.setText(id);
+        //Receive the note of the button that was clicked
+        Intent intent = getIntent();
+
+        //THIS IS NULL FOR SOME REASON WHEN FIRST RUN!
+        receivedNote = noteDAO.getItemById(intent.getIntExtra("noteID", 0));
+
+        System.out.println("Note data received");
+        System.out.println("Received note = "  + receivedNote);
+
+        //Display note info
+        text = findViewById(R.id.noteText);
+        text.setText(receivedNote.getText());
+
+        title = findViewById(R.id.titleText);
+        title.setText(receivedNote.getTitle());
+
+        System.out.println("Note data displayed");
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        System.out.println("Back Button Pressed");
+
+        //Save text in textbox to note, send note to database
+        receivedNote.setText(text.getText().toString());
+        receivedNote.setTitle(title.getText().toString());
+
+        //Put the new note in the DAO
+        noteDAO = appDatabase.getNoteDAO();
+        noteDAO.updateData(receivedNote);
+
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+        finish();
     }
 }
